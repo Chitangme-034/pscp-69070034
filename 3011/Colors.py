7a5b6cca062dd3c0c9ae3eae6e88ca60fec3d459
Chitangme-034 <69070034@kmitl.ac.th>
2026-07-14 19:01:47 +0700

    add main

--- 3011/Colors.py
+++ 3011/Colors.py
@@ -3,7 +3,13 @@ def main():
     """Mix"""
     Color1 = input()
     Color2 = input()
-    if (Color1 == "Red" and Color2 == "Yellow") or (Color1 == "Yellow" and Color2 == "Red"):
+    if Color1 == "Red" and Color2 == "Red":
+        print("Red")
+    elif Color1 == "Yellow" and Color2 == "Yellow":
+        print("Yellow")
+    elif Color1 == "Blue" and Color2 == "Blue":
+        print("Blue")
+    elif(Color1 == "Red" and Color2 == "Yellow") or (Color1 == "Yellow" and Color2 == "Red"):
         print("Orange")
     elif (Color1 == "Red" and Color2 == "Blue") or (Color1 == "Blue" and Color2 == "Red"):
         print("Violet")
