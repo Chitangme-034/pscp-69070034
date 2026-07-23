e069369e4bfb11a01cd7200c1777ae2fed8dd93d
Chitangme-034 <69070034@kmitl.ac.th>
2026-07-23 22:32:19 +0700

    add main

--- 3022/Temperature.py
+++ 3022/Temperature.py
@@ -4,17 +4,17 @@ def main():
     temp = float(input())
     start = input()
     end = input()
-    if start == "F":
+    if start == "F":                    # F to C
         temp = (temp - 32) * 5 / 9
-    elif start == "K":
+    elif start == "K":                  # K to C
         temp -= 273.15
-    elif start == "R":
+    elif start == "R":                  # R to C
         temp = temp * 5 / 9 - 273.15
-    if end == "F":
+    if end == "F":                      # C to F
         temp = temp * 9 / 5 + 32
-    elif end == "K":
+    elif end == "K":                    # C to K
         temp += 273.15
-    elif end == "R":
+    elif end == "R":                    # C to R
         temp = (temp + 273.15) * 9 / 5
     print(f"{temp:.2f}")
 main()
