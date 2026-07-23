c4e89dd39ee93fa1b5276bd2ab156d2033af1e2a
Chitangme-034 <69070034@kmitl.ac.th>
2026-07-23 19:57:20 +0700

    add main

--- 3022/Temperature.py
+++ 3022/Temperature.py
@@ -1,24 +1,20 @@
 """Temperature"""
 def main():
     """Convert temperature"""
-    temperature = float(input())
-    from_unit = input()
-    to_unit = input()
-    if from_unit == "C":        #Celsius
-        celsius = temperature
-    elif from_unit == "F":      #Fahrenheit
-        celsius = (temperature - 32) * 5 / 9
-    elif from_unit == "K":      #Kelvin
-        celsius = temperature - 273.15
-    else:                       # R / Rankine
-        celsius = (temperature * 5 / 9) - 273.15
-    if to_unit == "C":
-        answer = celsius
-    elif to_unit == "F":
-        answer = celsius * 9 / 5 + 32
-    elif to_unit == "K":
-        answer = celsius + 273.15
-    else:
-        answer = (celsius + 273.15) * 9 / 5
-    print(f"{answer:.2f}")
+    temp = float(input())
+    start = input()
+    end = input()
+    if start == "F":
+        temp = (temp - 32) * 5 / 9
+    elif start == "K":
+        temp -= 273.15
+    elif start == "R":
+        temp = temp * 5 / 9 - 273.15
+    if end == "F":
+        temp = temp * 9 / 5 + 32
+    elif end == "K":
+        temp += 273.15
+    elif end == "R":
+        temp = (temp + 273.15) * 9 / 5
+    print(f"{temp:.2f}")
 main()
