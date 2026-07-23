43a97ffa891f83861037c91494867c7fea89fb3f
Chitangme-034 <69070034@kmitl.ac.th>
2026-07-23 16:53:49 +0700

    Update หาร 10.py

--- 3042/หาร 10.py
+++ 3042/หาร 10.py
@@ -3,7 +3,7 @@ def main():
     """Divide by 10"""
     number = int(input())
     while number >= 0:
-        if number % 10 == 0:
+        if not number % 10:
             print(number, end=" ")
         number -= 1
 main()
