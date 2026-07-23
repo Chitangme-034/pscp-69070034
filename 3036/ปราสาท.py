93953d3a4fbce61873205cc98796d25796efb097
Chitangme-034 <69070034@kmitl.ac.th>
2026-07-23 18:06:11 +0700

    Update ปราสาท.py

--- 3036/ปราสาท.py
+++ 3036/ปราสาท.py
@@ -1,14 +1,14 @@
 """Palace"""
 def main():
     """brick"""
-    x = int(input())
-    floor = 1
-    while floor ** 2 < x:
+    x = int(input())        # room number
+    floor = 1               # start from 1st floor
+    while floor ** 2 < x:   # find the first floor where floor² is greater than or equal to x
         floor += 1
-    far = (floor**2) - x
-    room = 2*(floor) - 1
-    if not far % 2:
+    far = (floor**2) - x    # calculate how far x is from the last room on this floor
+    room = 2*(floor) - 1    # calculate the number of rooms on this floor
+    if not far % 2:         # distance is even
         print(room - 1)
-    else:
+    else:                   # distance is odd
         print(room - 2)
 main()
