2d6ce435d7922d704ae60fbecb9e4544b158475b
Chitangme-034 <69070034@kmitl.ac.th>
2026-07-23 17:42:56 +0700

    Update ปราสาท.py

--- 3036/ปราสาท.py
+++ 3036/ปราสาท.py
@@ -5,10 +5,10 @@ def main():
     floor = 1
     while floor ** 2 < x:
         floor += 1
-        far = (floor**2) - x
-        room = (2*floor) - 1
-        if not far % 2:
-            print(room - 1)
-        else:
-            print(room - 2)
+    far = (floor**2) - x
+    room = 2*(floor) - 1
+    if not far % 2:
+        print(room - 1)
+    else:
+        print(room - 2)
 main()
