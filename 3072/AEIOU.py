88a18fb5d32f6a8c8b5f2258352e151b2bbc12cf
Chitangme-034 <69070034@kmitl.ac.th>
2026-08-06 23:48:50 +0700

    add main

--- 3072/AEIOU.py
+++ 3072/AEIOU.py
@@ -1,10 +1,14 @@
 """Count Vowels"""
 def main():
-    """Count vowels in a sentence."""
-    text = input()
-    vowels = []
+    """Count each vowel"""
+    text = input().lower()
+    vowels = ["a", "e", "i", "o", "u"]
+    count = [0, 0, 0, 0, 0]
     for letter in text:
-        if letter.lower() in "aeiou":
-            vowels.append(letter)
-    print(len(vowels))
+        if letter in vowels:
+            index = vowels.index(letter)
+            count[index] += 1
+    for index in range(5):
+        if count[index]:
+            print(vowels[index], ":", count[index])
 main()
