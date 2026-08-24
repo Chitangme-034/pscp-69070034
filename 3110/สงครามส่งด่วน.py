e34440e165a13b4772fe9b8408941adff40f0e14
Chitangme-034 <69070034@kmitl.ac.th>
2026-08-24 22:16:14 +0700

    add main

---
+++ 3110/สงครามส่งด่วน.py
@@ -0,0 +1,29 @@
+"""Express Delivery"""
+def main():
+    """Calculate delivery fee"""
+    start, destination = input().split()
+    weight = float(input())
+    if start == "BKK" and destination == "CNX":
+        base = 10
+        rate = 30
+    elif start == "CNX" and destination == "UBP":
+        base = 15
+        rate = 40
+    elif start == "UBP" and destination == "BKK":
+        base = 20
+        rate = 40
+    elif start == "BKK" and destination == "PKT":
+        base = 25
+        rate = 50
+    elif start == "PKT" and destination == "CNX":
+        base = 30
+        rate = 60
+    elif start == "UBP" and destination == "PKT":
+        base = 40
+        rate = 70
+    else:
+        print("Error")
+        return
+    price = base + weight * rate
+    print(price)
+main()
