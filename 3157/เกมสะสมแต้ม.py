e8825d6a24cbe896283559cdc89e482e29492e66
Chitangme-034 <69070034@kmitl.ac.th>
2026-08-28 14:22:48 +0700

    add main

---
+++ 3157/เกมสะสมแต้ม.py
@@ -0,0 +1,13 @@
+"""Game Score"""
+def main():
+    """Final score"""
+    n = int(input())
+    score = 0
+    for _ in range(n):
+        command = input()
+        if command == "+":
+            score += 10
+        elif command == "-":
+            score -= 5
+    print(score)
+main()
