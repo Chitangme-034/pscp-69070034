f389c3ce5999b3250c696fba6ddd128b24044ff0
Chitangme-034 <69070034@kmitl.ac.th>
2026-09-02 12:49:50 +0700

    add main

--- 3135/ของขวัญและขโมย.py
+++ 3135/ของขวัญและขโมย.py
@@ -1,19 +1,20 @@
 """Festival Gift"""
 def main():
     """Examine the gift"""
-    n, k, t = input().split()
-    n = int(n)
-    k = int(k)
-    t = int(t)
+    n, k, t = map(int, input().split())
     current = 1
     count = 1
-    while True:
-        current = (current + k - 1) % n + 1
-        if current == t:
+
+    if t == 1:
+        print(1)
+    else:
+        while True:
+            current = (current + k) % n
+            if current == 1:
+                print(count)
+                break
             count += 1
-            break
-        if current == 1:
-            break
-        count += 1
-    print(count)
+            if current == t:
+                print(count)
+                break
 main()
