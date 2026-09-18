976b51bc9c785d7ff310bc26164037dfdb9fbf35
Chitangme-034 <69070034@kmitl.ac.th>
2026-09-18 15:00:22 +0700

    add main

--- 3296/RGB Mixed.py
+++ 3296/RGB Mixed.py
@@ -1,17 +1,19 @@
-"""RGB"""
-def mixColor(c1, c2):
-    """Mix two color value"""
+"""RGB Mixed"""
+def mix_color(c1, c2):
+    """Two color values"""
     return (c1 + c2) // 2
-def mixRGB(r1, g1, b1, r2, g2, b2):
-    """Mix two RGB colors"""
-    rMix = mixColor(r1, r2)
-    bMix = mixColor(b1, b2)
-    gMix = mixColor(g1, g2)
-    return(rMix, gMix, bMix)
+def mix_rgb(color1, color2):
+    """Two RGB colors"""
+    r1, g1, b1 = color1
+    r2, g2, b2 = color2
+    r_mix = mix_color(r1, r2)
+    g_mix = mix_color(g1, g2)
+    b_mix = mix_color(b1, b2)
+    return r_mix, g_mix, b_mix
 def main():
     """Mixed RGB color"""
-    r1, g1, b1 = map(int, input().split())
-    r2, g2, b2 = map(int, input().split())
-    rMix, gMix, bMix = mixRGB(r1, g1, b1, r2, g2, b2)
-    print(rMix, gMix, bMix)
+    color1 = tuple(map(int, input().split()))
+    color2 = tuple(map(int, input().split()))
+    r_mix, g_mix, b_mix = mix_rgb(color1, color2)
+    print(r_mix, g_mix, b_mix)
 main()
